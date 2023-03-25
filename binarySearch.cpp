30e4664c3945fc54bd5f8b83ed00e1374e6ee8f8
Varshitha Bittu <91466661+varshithabittu@users.noreply.github.com>
2023-03-25 17:44:30 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[],int s,int e,int target)
+{
+    int mid = s+(e-s)/2;
+    if(s>e)
+    {
+        return -1;
+    }
+   if(arr[mid]>target)
+        return binarySearch(arr,s,mid-1,target);
+    else
+        return binarySearch(arr,mid+1,e,target);
+}
+int main() {
+    int arr[5]={1,2,3,4,5};
+    int target;
+    cin>>target;
+    int ans = binarySearch(arr,0,4,target);
+    if(ans!=-1)
+        cout << "Found at" << ans << endl;
+    else
+        cout<<"Not Found"<<endl;
+    return 0;
+}
